ac5c0cde561908ef203e461b5c022c0d7c1dd053
Maria Shevchenko <shevtma@yandex.ru>
2022-12-19 00:46:50 +0300

    added secretsanta e2e test

---
+++ cypress/e2e/secretsanta.cy.js
@@ -0,0 +1,10 @@
+describe("Sercet Santa spec", () => {
+  beforeEach(() => {
+    cy.visit("https://staging.lpitko.ru/");
+  });
+
+  it('Проверяем, что на странице есть текст "тайный санта." ', () => {
+    cy.get("span.txt-h3--semi").first().should("have.text", "тайный санта.");
+  });
+});
+
